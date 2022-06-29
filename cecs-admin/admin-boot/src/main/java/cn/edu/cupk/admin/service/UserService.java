0be307feb445f22fcd13d9cbd24e66da3be7465f
赵希奥 <ckxgzxa@qq.com>
2022-06-30 05:58:17 +0800

    add cecs-common

---
+++ cecs-admin/admin-boot/src/main/java/cn/edu/cupk/admin/service/UserService.java
@@ -0,0 +1,16 @@
+package cn.edu.cupk.admin.service;
+
+import cn.edu.cupk.admin.dto.UserAuthDTO;
+import cn.edu.cupk.admin.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @author 赵希奥
+ * @date 2022/6/29 11:37
+ * @github https://github.com/CKXGZXA
+ * @gitee https://gitee.com/ckxgzxa
+ * @description:
+ */
+public interface UserService extends IService<User> {
+    UserAuthDTO getAuthInfoByUsername(String username);
+}
