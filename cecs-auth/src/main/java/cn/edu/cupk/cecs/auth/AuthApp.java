1660d04fe54ecde89b3ae5b2a9bbedca136369ca
赵希奥 <ckxgzxa@qq.com>
2022-07-02 02:28:31 +0800

    fix some issues

--- cecs-auth/src/main/java/cn/edu/cupk/cecs/auth/AuthApp.java
+++ cecs-auth/src/main/java/cn/edu/cupk/cecs/auth/AuthApp.java
@@ -1,7 +1,5 @@
 package cn.edu.cupk.cecs.auth;
 
-// import org.mybatis.spring.annotation.MapperScan;
-import cn.edu.cupk.admin.api.UserFeignClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
